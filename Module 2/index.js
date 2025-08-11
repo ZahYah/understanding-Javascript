2c6990e01539def456f6d2e58fd1b9cef471f9aa
ZahYah <adebayoisaiah256@gmail.com>
2025-08-11 16:04:02 +0100

    Completed Calculator and RPS game

---
+++ Module 2/index.js
@@ -0,0 +1,5 @@
+let  fahrenheit;
+let celsius;
+// fahrenheit = (-5 * 9/ 5) + 32;
+celsius = (86 - 32 ) * 5 /9;
+console.log(celsius);
---
+++ Module 5/execise.js
@@ -0,0 +1,6 @@
+let name = 'Isaiah';
+// console.log(`My name is ${name}`);
+let cost = 5 + (2 *3) + 9;
+let tax = cost * 0.1;
+let total = cost + tax;
+console.log(`Total cost: $$${total}`);
